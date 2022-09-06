134759c9cad414a94130b083195fe891a713e2c1
atharva-2001 <aryaa_1@rknec.edu>
2022-09-06 06:31:36 +0530

    Add test class and reformat test file

--- test_hdf.py
+++ test_hdf.py
@@ -1,27 +1,23 @@
-from pandas import array
-import pytest
 import numpy as np
-import tables
+import pytest
 
-@pytest.mark.share_hdf
-@pytest.mark.parametrize("n_points", [50, 100])
-def test_arr_mul(
-    n_points, refdata
-):
-    # reference data is used internally
 
-    arr = np.array([1,2,3])*n_points
+@pytest.mark.parametrize("n_points", [150, 200])
+def test_arr_mul(n_points):
+    arr = np.array([4, 5, 6]) * n_points
     return arr
+
 
 @pytest.mark.share_hdf
 @pytest.mark.parametrize("n_points", [150, 200])
-def test_arr_mul2(
-    n_points, refdata
-):  
-    arr = np.array([4,5,6])*n_points
+def test_arr_mul2(n_points):
+    arr = np.array([4, 5, 6]) * n_points
     return arr
 
 
+@pytest.mark.share_hdf
 class TestFoo:
-    ...
-
+    @pytest.mark.parametrize("n_points", [150, 200])
+    def test_arr_mul3(self, n_points):
+        arr = np.array([4, 5, 6]) * n_points
+        return arr
